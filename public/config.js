89526249d28b81b586d3e08bb703d5a22b119b3d
ViV IDC财务管理系统 <11467102@qq.com>
2021-09-15 14:26:10 +0800

    修正配置文件

--- public/config.js
+++ public/config.js
@@ -5,8 +5,6 @@ window.serverConfig = {
   APP_NAME: '开发者中心',
   // 网站LOGO(绝对路径)
   APP_LOGO_URL: 'http://muyucms.com/favicon.ico',
-  // 前台模块URL(绝对路径)
-  APP_INDEX_URL: 'http://muyucms.com/index/',
   // 系统版本
   APP_VERSION: '1.0'
 }
